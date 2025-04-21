a456d6a823886cb26b187eeba5474f3eb07c1ca4
Jinthan10 <153614490+Jinthan10@users.noreply.github.com>
2025-04-21 18:52:36 +0530

    Update Contactus.js

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -25,7 +25,8 @@ const Contactus = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://myloader-54490939.us-east-1.elb.amazonaws.com/contactus', formData);
+      #const response = await axios.post('http://myloader-54490939.us-east-1.elb.amazonaws.com/contactus', formData);
+      const response = await axios.post('https://backend-936800529124.us-central1.run.app', formData);
       console.log('Form submission response:', response.data);
 
       // Display success message and clear form
